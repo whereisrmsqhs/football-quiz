0c985aed1bd1c85b8532ab4e5d47a8fd3198de8e
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-04-03 11:24:51 +0900

    fix, add extension to uploaded images

--- src/app.js
+++ src/app.js
@@ -1,27 +1,121 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var cors = require("cors");
-var app = (0, express_1.default)();
-var port = process.env.PORT || 3000;
-var corsOption = {
-    origin: ["http://localhost:3001"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowHeaders: ["Content-Type", "Authorization"],
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const db = require("./mysql.js");
+const path = require("path");
+const app = express();
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const port = process.env.PORT || 3001;
+
+const corsOption = {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowHeaders: ["Content-Type", "Authorization"],
 };
+
 app.use(cors(corsOption));
-app.get("/toto", function (req, res) {
-    res.send("Hello toto");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./src/uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf() + "." + file.mimetype.split("/")[1]);
+    },
+  }),
 });
-var quizBlock = {
+
+const posts = [
+  {
     id: 1,
-    title: "Title",
-    brief_explain: "설명",
-    thumbnail: "/",
-};
-app.get("/quiz", function (req, res) {
-    res.send(quizBlock);
+    author: "관리자",
+    title: "게시판1",
+    time: "2024-03-27, 14:17",
+  },
+  {
+    id: 2,
+    author: "관리자",
+    title: "게시판2",
+    time: "2024-03-27, 14:23",
+  },
+];
+app.get("/quiz", (req, res) => {
+  db.query(`SELECT * from quiz_collection`, function (error, quiz_collection) {
+    if (error) {
+      throw error;
+    }
+    const quiz_list = [];
+    quiz_collection.map((quiz) => {
+      quiz_list.push({
+        id: quiz.pk,
+        title: quiz.name,
+        brief_explain: quiz.quiz_rule,
+        thumbnail: quiz.thumbnail,
+      });
+    });
+    res.send(quiz_list);
+  });
 });
+
+app.get("/community", (req, res) => {
+  res.send(posts);
+});
+
+app.get("/quiz/:quizId", (req, res) => {
+  res.send("success");
+});
+
+app.post("/quiz/post", upload.single("quiz_thumbnail"), (req, res) => {
+  const post = req.body;
+  const thumbnail = req.file;
+  console.log(thumbnail);
+  db.query(
+    `INSERT INTO quiz_collection (name, type, thumbnail, quiz_rule, user_id) 
+      VALUES (?, ?, ?, ?, ?);`,
+    [post.quiz_title, post.quiz_type, thumbnail?.path, post.quiz_explain, 2],
+    function (error, result) {
+      if (error) {
+        throw error;
+      }
+      const quiz_collection_id = result.insertId;
+      const quiz_info = JSON.parse(post.quiz_info);
+      console.log(quiz_collection_id);
+
+      // 쿼리를 순차적으로 실행하고 모든 쿼리가 완료된 후에 응답을 보냄
+      const promises = quiz_info.map((info) => {
+        return new Promise((resolve, reject) => {
+          db.query(
+            `INSERT INTO type1 (club_array, answer, quiz_collection_pk) VALUES (?, ?, ?);`,
+            [info.team, info.answer, quiz_collection_id],
+            function (error, result) {
+              if (error) {
+                reject(error);
+              } else {
+                resolve();
+              }
+            }
+          );
+        });
+      });
+
+      Promise.all(promises)
+        .then(() => {
+          res.writeHead(302, { Location: `/quiz` });
+          res.end();
+        })
+        .catch((error) => {
+          console.error("Error executing queries:", error);
+          res.status(500).send("Error executing queries");
+        });
+    }
+  );
+});
+
 app.listen(port, function () {
-    console.log("App is listening on port ".concat(port, " !"));
+  console.log(`App is listening on port ${port} !`);
 });
